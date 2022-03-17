6854c8b09eb2fb73954676d5aba5ff3703f97f0f
shenbaga-praburaj <shenbaga.praburaj@freshworks.com>
2022-03-17 12:36:12 +0530

    component name change

--- tests/integration/components/text-input-field-test.js
+++ tests/integration/components/text-input-field-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | valid-input', function(hooks) {
+module('Integration | Component | text-input-field', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{valid-input}}`);
+    await render(hbs`{{text-input-field}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#valid-input}}
+      {{#text-input-field}}
         template block text
-      {{/valid-input}}
+      {{/text-input-field}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
