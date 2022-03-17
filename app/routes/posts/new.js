c6519c4d5717fa65a3b973022af4f85620119524
shenbaga-praburaj <shenbaga.praburaj@freshworks.com>
2022-03-17 11:57:43 +0530

    validations for new post

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -1,4 +1,12 @@
-import Route from '@ember/routing/route';
+import Route from '@ember/routing/route'
+import { get, set } from '@ember/object'
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
+    store: service(),
+
+    setupController(controller, model) {
+        let newpost = this.store.createRecord('post')
+        set(controller, 'newPost', newpost)
+    }
 });
