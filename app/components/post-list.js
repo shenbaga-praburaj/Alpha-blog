1614ad70f09616ec98b8b68062392f5360e0e652
shenbaga-praburaj <shenbaga.praburaj@freshworks.com>
2022-03-17 22:59:48 +0530

    filter by author by using ember power select

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -1,4 +1,34 @@
 import Component from '@ember/component';
+import { computed, get, set } from '@ember/object';
 
 export default Component.extend({
+
+    authors: computed('posts.@each.author', {
+        get() {
+            const array = ['All']
+            this.posts.forEach(post => {
+                if(array.indexOf(post.author) === -1 && post.author) {
+                    array.push(post.author)
+                }
+                    
+            })
+            set(this, 'author', array[0])
+            return array
+        }
+    }),
+
+    filteredPosts: computed('author', {
+        get() {
+            if(this.author !== 'All') {
+                return this.posts.filter(post => post.author === this.author)
+            }
+            return this.posts
+        }
+    }),
+
+    actions: {
+        onChange(author) {
+            set(this, 'author', author)
+        }
+    }
 });
