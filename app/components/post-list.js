ab63cda2d10d8fbfb15d559ab41e0faac0bc5b82
shenbaga-praburaj <shenbaga.praburaj@freshworks.com>
2022-03-21 16:11:36 +0530

    Usage of Set for Unique authors and getter removed

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -3,27 +3,18 @@ import { computed, get, set } from '@ember/object';
 
 export default Component.extend({
 
-    authors: computed('posts.@each.author', {
-        get() {
-            const array = ['All']
-            this.posts.forEach(post => {
-                if(array.indexOf(post.author) === -1 && post.author) {
-                    array.push(post.author)
-                }
-                    
-            })
-            set(this, 'author', array[0])
-            return array
-        }
+    authors: computed('posts.@each.author', function() {
+        const array = ['All', ...new Set(this.posts.filter(post => post.author).map(post => post.author))]
+
+        set(this, 'author', array[0])
+        return array
     }),
 
-    filteredPosts: computed('author', {
-        get() {
-            if(this.author !== 'All') {
-                return this.posts.filter(post => post.author === this.author)
-            }
-            return this.posts
+    filteredPosts: computed('author', function() {
+        if(this.author !== 'All') {
+            return this.posts.filter(post => post.author === this.author)
         }
+        return this.posts
     }),
 
     actions: {
